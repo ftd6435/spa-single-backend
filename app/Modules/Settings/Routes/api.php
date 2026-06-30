89b63aa70b083cf70d3716ff50c4e52807b0329c
Pathé PK Diallo <mamadoupathediallo71@gmail.com>
2026-06-30 14:52:30 +0000

    Excluded tags and categories GET methods out of the auth:sanctum

--- app/Modules/Settings/Routes/api.php
+++ app/Modules/Settings/Routes/api.php
@@ -4,11 +4,21 @@ use App\Modules\Settings\Controllers\CategoryController;
 use App\Modules\Settings\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('v1/admin')->group(function () {
+    Route::get('/categories/{category}', [CategoryController::class, 'show']);
+    Route::get('/categories', [CategoryController::class, 'index']);
+
+    Route::get('/tags/{tag}', [TagController::class, 'show']);
+    Route::get('/tags', [TagController::class, 'index']);
+});
+
 // Define API routes for Settings module here
 Route::middleware('auth:sanctum')->prefix('v1/admin')->group(function () {
     Route::get('/categories/{id}/category', [CategoryController::class, 'switchStatus']);
-    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('categories', CategoryController::class)->except(['show', 'index']);
+
+
 
     Route::get('/tags/{id}/tag', [TagController::class, 'switchStatus']);
-    Route::apiResource('tags', TagController::class);
+    Route::apiResource('tags', TagController::class)->except(['show', 'index']);
 });
