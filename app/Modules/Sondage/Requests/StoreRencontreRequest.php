420c4bd843e6a3a06c64422a599bc2602b83615f
Pathé PK Diallo <mamadoupathediallo71@gmail.com>
2026-07-15 16:07:57 +0000

    Removed the rencontre default options

--- app/Modules/Sondage/Requests/StoreRencontreRequest.php
+++ app/Modules/Sondage/Requests/StoreRencontreRequest.php
@@ -18,9 +18,7 @@ class StoreRencontreRequest extends FormRequest
             'home_team_id'    => ['required', 'integer', 'exists:equipes,id', 'different:away_team_id'],
             'away_team_id'    => ['required', 'integer', 'exists:equipes,id'],
             'init_sondage_id' => ['required', 'integer', 'exists:init_sondages,id'],
-            'phase'           => ['required', 'string', Rule::in([
-                'groupe_stage', 'round_of_32', 'round_of_16', 'quarter_final', 'semi_final', 'final',
-            ])],
+            'phase'           => ['required', 'string', 'max:30'],
             'date_rencontre'  => ['nullable', 'date'],
             'heure_rencontre' => ['nullable', 'date_format:H:i'],
             'team_winner_id'  => ['nullable', 'integer', 'exists:equipes,id'],
--- app/Modules/Sondage/Requests/UpdateRencontreRequest.php
+++ app/Modules/Sondage/Requests/UpdateRencontreRequest.php
@@ -18,9 +18,7 @@ class UpdateRencontreRequest extends FormRequest
             'home_team_id'    => ['sometimes', 'integer', 'exists:equipes,id', 'different:away_team_id'],
             'away_team_id'    => ['sometimes', 'integer', 'exists:equipes,id'],
             'init_sondage_id' => ['sometimes', 'integer', 'exists:init_sondages,id'],
-            'phase'           => ['sometimes', 'string', Rule::in([
-                'groupe_stage', 'round_of_32', 'round_of_16', 'quarter_final', 'semi_final', 'final',
-            ])],
+            'phase'           => ['sometimes', 'string', 'max:30'],
             'date_rencontre'  => ['sometimes', 'nullable', 'date'],
             'heure_rencontre' => ['sometimes', 'nullable', 'date_format:H:i'],
             'team_winner_id'  => ['sometimes', 'nullable', 'integer', 'exists:equipes,id'],
